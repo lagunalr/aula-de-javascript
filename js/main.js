2c002d2390ccd8c4ca19e55dfd79cbd7fee31ae5
Your Name <you@example.com>
2021-03-26 17:32:34 -0300

    testaando o javasript e variaveis

---
+++ js/main.js
@@ -0,0 +1,27 @@
+
+
+var lista = ["Alemanha", "Inglaterra", "Escócia"];
+lista.push("Polônia");
+lista.pop("Inglaterra");
+console.log(lista);
+
+
+
+
+
+
+//var idade = prompt("Qual sua idade");
+//if(idade >= 18);{
+
+    //alert("maior de idade prossiga");
+//}else{
+    //alert("menor de idade");
+//};
+//var nome = "Matheus Laguna";
+//var idade = "29"
+//var frase = "Japão é o melhor time do mundo"
+
+//console.log(nome);
+//console.log(idade);
+//console.log(frase.replace("Japão","Brasil"));
+//alert(frase.replace("Japão","Brasil"));
